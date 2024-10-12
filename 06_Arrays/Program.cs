07f69af3ebeaa24ddeea82dfb1b2498d27c5cdf7
Yasin Çetin <yasincetin0678@gmail.com>
2024-10-12 19:13:51 +0300

    Arrays

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region temel dizi ornekleri
+
+            //string[] colors = new string[4];
+            //colors[0] = "Kirmizi";
+            //colors[1] = "Sari";
+            //colors[2] = "Beyaz";
+            //colors[3] = "Mavi";
+
+            //Console.WriteLine(colors[2]);
+
+            //string[] cities = new string[5];
+            //cities[0] = "Milano";
+            //cities[1] = "Budapeste";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Uskup";
+
+            //Console.WriteLine(cities[4]);
+
+            //int[] numbers = new int[10];
+            //numbers[0] = 50;
+            //numbers[1] = 48;
+            //numbers[2] = 698;
+            //numbers[3] = 24;
+            //numbers[7] = 748;
+            //Console.WriteLine(numbers[7]);
+
+            //string[] cities = { "Prag", "Roma", "Atina", "Ankara", "Bursa" };
+            //Console.WriteLine(cities[2]);
+
+            #endregion
+
+            #region Dizideki Tum Elemanlari Listeleme
+
+            //string[] colors = { "Sari", "Kirmizi", "Beyaz", "Mavi", "Yesil", "Turuncu", "Pembe" };
+
+            //for (int i = 0; i < colors.Length; i++) {
+
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //int[] numbers = { 4, 85, 96, 74, 125, 635, 488, 520, 7456, 2365, 1120 };
+            //for (int i = 0; i < numbers.Length; i++) 
+            //{
+            //    if (numbers[i] % 3 == 0) 
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+            //}
+
+            //char[] chars = { 'a', 'b', 'c', '*', '/', '-' };
+            //for (int i = 0; i < chars.Length; i++) {
+            //    Console.WriteLine(chars[i]);
+            //}
+
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+            //int maximumnumber = myArray[0];
+            //for (int i = 0; i < myArray.Length; i++) {
+
+            //    if (myArray[i] > maximumnumber)
+            //    {
+            //        maximumnumber = myArray[i];
+            //    }
+
+            //}
+            //Console.WriteLine(maximumnumber);
+
+            //string[] persons = { "ali", "ahmet", "ayse", "buse", "cem", "deniz" };
+            //Console.WriteLine(persons.Length);
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
